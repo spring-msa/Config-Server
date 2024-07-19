a5fc4539c5741578502c77ec31ddc737c49c2b7b
Namju Kim <cmsong111@naver.com>
2024-07-19 22:39:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Spring Cloud Config Server
+
+클라우드 환경에서 스프링 환경 설정을 외부에 두고 관리할 수 있는 서버입니다.
---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
+}
+
+
+group = "com.namju"
+version = "0.0.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springBootAdminVersion"] = "3.3.2"
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+    // Spring Cloud Config Server
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+
+    // Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Spring Boot Admin
+    implementation("de.codecentric:spring-boot-admin-starter-client")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
